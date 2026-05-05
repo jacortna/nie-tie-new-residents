2f997e570867fe47282aa285d419a4af77f809d9
javier cortes navarro <jacortna@gmail.com>
2026-05-05 15:07:30 +0000

    Visual edits

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
             Encuentra el{" "}<span className="text-[#C9A800] hidden">camino</span><br />
             <span className="text-[#C9A800] hidden">para hacerlo realidad.</span>
           </h1>
-          <p className="text-[#555] text-base leading-relaxed">T
+          <p className="text-[#555] text-base leading-relaxed">TE AYUDAMOS 
 
 
           </p>
