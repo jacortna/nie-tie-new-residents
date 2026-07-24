87eecbbcd2132e0afaad2370ee906c395ffdea71
jacortna <tucorreo@gmail.com>
2026-07-24 06:55:16 +0200

    Updates

---
+++ assets/index-BMD8Vrqn.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Ba3Lv2jK.js","assets/index-CFW_YwJb.js","assets/index-DX1j8eGU.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CFW_YwJb.js";const o=r("Share",{web:()=>t(()=>import("./web-Ba3Lv2jK.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
--- assets/index-C6iAuE5F.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-3cJxMoeY.js","assets/index-BEQ042n7.js","assets/index-DX1j8eGU.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BEQ042n7.js";const o=r("Share",{web:()=>t(()=>import("./web-3cJxMoeY.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
